cb37b68e4896b0570cf1a00d771fb203eb981d1b
Raghav Sood <raghavsood@gmail.com>
2015-01-10 18:03:49 +0530

    Fixed issue with finding tags in news feed posts with comments

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -21,6 +21,9 @@ function tagger() {
 		console.log(result);
 		if(result != null && result.length > 0) {
 			var link = groupLinkFinder($(this))
+			if(link==-1) {
+				return;
+			}
 			var text = $(this).html().replace(/<[^>]*>/g, "");
 			if($(location).attr('pathname').indexOf("/search/") != -1) {
     			$(this).html( text.replace( regExp, '<a class="tagtext" href="' + link + '?query=$1">$1</a>' ) );
@@ -34,11 +37,24 @@ function tagger() {
 function groupLinkFinder(startingNode) {
 	//var linkexp = /\/groups(.*?)permalink\//g;
 	var linkexp= /\/groups\/(.*?)\//;
+	var postDivExtra = startingNode.parent().parent().parent().parent();
 	var postDiv = startingNode.parent().parent();
 	var divHTML = postDiv.html();
+	var divHTMLExtra = postDivExtra.html();
+	console.log(divHTML);
 	var link = linkexp.exec(divHTML);
-	console.log("/groups" + link[1] + "/");
-
-	return "/groups/" + link[1] + "/";
+	if(link!=null && link.length > 0) {
+		console.log("/groups/" + link[1] + "/");
+		return "/groups/" + link[1] + "/";
+	} else {
+		link = linkexp.exec(divHTMLExtra);
+		console.log(divHTMLExtra);
+		if(link!=null && link.length > 0) {
+			console.log("/groups/" + link[1] + "/");
+			return "/groups/" + link[1] + "/";
+		} else {
+			return -1;
+		}
+	}
 
 }
